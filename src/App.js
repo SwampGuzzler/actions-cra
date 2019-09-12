71abdb258650d5571be6cf40ff1dda85b7de47d9
Lucas Cotner <lc07@uw.edu>
2019-09-12 17:12:26 -0400

    Added deploy job, updated component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>Let's roll some actions!</p>
+        <h1>SwampGuzzer IO</h1>
+        <p>(via some actions)</p>
       </header>
     </div>
   );
